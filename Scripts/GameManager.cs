c62b04104ea91fd4449277fe931bd59485b46ac8
rickygow <rickygow@gmail.com>
2016-06-14 00:10:03 +0700

    Game Manager v0.0.1
    Cuma bisa fetch data XML, buat test

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class GameManager : MonoBehaviour {
+
+	private PlayerStatus myPlayer;
+	private PlayerStatus myEnemy;
+
+	void Start () {
+		myPlayer = new PlayerStatus (1);
+		myPlayer.SavePlayerStatus ();
+		Debug.Log (myPlayer.GetName ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

9ec5e904f6fff3f4b147672500a1406239c230e8
rickygow <rickygow@gmail.com>
2016-06-14 00:06:20 +0700

    XML Serialization
    Save data sama penerapan awal Class pakai XML
    akan dilanjutkan untuk Weapon, Equipment, dan Skill (Belum Pasti)

---
+++ Scripts/GamePlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class GamePlayer {
+
+	[XmlAttribute("slot")]
+	public int slot;
+
+	public string playerName;
+	public string playerClass;
+
+	public int head;
+	public int armor;
+	public int wep1;
+	public int wep2;
+	public int str;
+	public int dur;
+	public int agi;
+	public int intel;
+
+	/*
+	public PlayerClass myClass;
+	public PlayerWeapon myWeapon1;
+	public PlayerWeapon myWeapon2;
+
+	*/
+
+}
---
+++ Scripts/PlayerClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+public class PlayerClass {
+	
+	[XmlAttribute("name")]
+	public string className;
+	public int bHp;
+	public int bMp;
+	public int bStr;
+	public int bDur;
+	public int bAgi;
+	public int bInt;
+
+}

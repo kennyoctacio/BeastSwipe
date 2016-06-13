f60813603f4b934bde907488cd84a479a91f5b2f
rickygow <rickygow@gmail.com>
2016-06-14 00:08:02 +0700

    Write dan Read XML untuk PlayerSaves dan Class
    untuk contoh XML nanti gua bikin foldernya

---
+++ Scripts/ClassContainer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+[XmlRoot("ClassLists")]
+public class ClassContainer {
+
+	[XmlArray("Classes"),XmlArrayItem("Class")]
+	public List<PlayerClass> playerClass;
+	//public PlayerClass[] playerClass;
+
+	public void Save(string path)
+	{
+		var serializer = new XmlSerializer(typeof(ClassContainer));
+		using(var stream = new FileStream(path, FileMode.Create))
+		{
+			serializer.Serialize(stream, this);
+		}
+	}
+
+	public static ClassContainer Load(string path)
+	{
+		var serializer = new XmlSerializer(typeof(ClassContainer));
+		using(var stream = new FileStream(path, FileMode.Open))
+		{
+			return serializer.Deserialize(stream) as ClassContainer;
+		}
+	}
+
+	//Loads the xml directly from the given string. Useful in combination with www.text.
+	public static ClassContainer LoadFromText(string text) 
+	{
+		var serializer = new XmlSerializer(typeof(ClassContainer));
+		return serializer.Deserialize(new StringReader(text)) as ClassContainer;
+	}
+}
---
+++ Scripts/PlayerContainer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+[XmlRoot("PlayerSaves")]
+public class PlayerContainer {
+
+	[XmlArray("PlayerStatus"),XmlArrayItem("Player")]
+	public GamePlayer[] Players;
+
+	public void Save(string path)
+	{
+		var serializer = new XmlSerializer(typeof(PlayerContainer));
+		using(var stream = new FileStream(path, FileMode.Create))
+		{
+			serializer.Serialize(stream, this);
+		}
+	}
+
+	public static PlayerContainer Load(string path)
+	{
+		var serializer = new XmlSerializer(typeof(PlayerContainer));
+		using(var stream = new FileStream(path, FileMode.Open))
+		{
+			return serializer.Deserialize(stream) as PlayerContainer;
+		}
+	}
+
+	//Loads the xml directly from the given string. Useful in combination with www.text.
+	public static PlayerContainer LoadFromText(string text) 
+	{
+		var serializer = new XmlSerializer(typeof(PlayerContainer));
+		return serializer.Deserialize(new StringReader(text)) as PlayerContainer;
+	}
+}
